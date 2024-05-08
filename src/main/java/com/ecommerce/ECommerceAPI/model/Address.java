157f4d350fc684e99dca7555f8c21aa0a2dfc810
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2024-05-08 15:11:28 +0200

    Creation of the E-Commerce REST API

---
+++ src/main/java/com/ecommerce/ECommerceAPI/model/Address.java
@@ -0,0 +1,65 @@
+package com.ecommerce.ECommerceAPI.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "addressLine1", nullable = false, length = 512)
+    private String addressLine1;
+
+    @Column(name = "addressLine2", length = 512)
+    private String addressLine2;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 75)
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
