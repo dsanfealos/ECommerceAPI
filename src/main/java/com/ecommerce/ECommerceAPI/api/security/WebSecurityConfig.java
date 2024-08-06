753d4b52b46ef34c2ed88e17887fee5ef28fe2e7
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2024-08-06 12:32:48 +0200

    Endpoints are secured by authentication by default. Required authentications on specific endpoints.

--- src/main/java/com/ecommerce/ECommerceAPI/api/security/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/ECommerceAPI/api/security/WebSecurityConfig.java
@@ -5,14 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
+/**
+ * Configurations of the security on endpoints
+ */
 @Configuration
 public class WebSecurityConfig {
 
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    /**
+     * Filter chain to configure security
+     * @param http The security project
+     * @return The chain built
+     * @throws Exception Throw an error configuring
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                .anyRequest().authenticated());
         return http.build();
     }
 }
