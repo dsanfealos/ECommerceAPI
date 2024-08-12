9322d39d37fdf0ff5a94ef4ce8cc571423f1436f
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2024-08-12 12:34:40 +0200

    Adding WebSockets to the application. WebSocketConfiguration created. Added preSend method to JWTRequestFilter.

---
+++ src/main/java/com/ecommerce/ECommerceAPI/api/security/WebSocketConfiguration.java
@@ -0,0 +1,59 @@
+package com.ecommerce.ECommerceAPI.api.security;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    private ApplicationContext context;
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSocketConfiguration(ApplicationContext context, JWTRequestFilter jwtRequestFilter) {
+        this.context = context;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOriginPatterns("**").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    private AuthorizationManager<Message<?>> makeMessageAuthorizationManager(){
+        MessageMatcherDelegatingAuthorizationManager.Builder messages =
+                new MessageMatcherDelegatingAuthorizationManager.Builder();
+        messages.
+                simpDestMatchers("/topic/user/**").authenticated()
+                .anyMessage().permitAll();
+        return messages.build();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        AuthorizationManager<Message<?>> authorizationManager = makeMessageAuthorizationManager();
+        AuthorizationChannelInterceptor authInterceptor = new AuthorizationChannelInterceptor(authorizationManager);
+        AuthorizationEventPublisher publisher = new SpringAuthorizationEventPublisher(context);
+        authInterceptor.setAuthorizationEventPublisher(publisher);
+        registration.interceptors(jwtRequestFilter, authInterceptor);
+    }
+}
