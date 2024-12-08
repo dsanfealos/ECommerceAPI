357a179e3c842d1c730263f4f6e22967c530c09c
Daniel Sanfelix <dsanfealos@gmail.com>
2024-12-08 15:14:24 +0100

    Added CRUD methods to OrderController. Method createOrder may be upgraded.

--- src/main/java/com/ecommerce/ECommerceAPI/api/controller/order/OrderController.java
+++ src/main/java/com/ecommerce/ECommerceAPI/api/controller/order/OrderController.java
@@ -1,13 +1,15 @@
 package com.ecommerce.ECommerceAPI.api.controller.order;
 
+import com.ecommerce.ECommerceAPI.api.model.OrderBody;
 import com.ecommerce.ECommerceAPI.model.LocalUser;
 import com.ecommerce.ECommerceAPI.model.WebOrder;
 import com.ecommerce.ECommerceAPI.service.OrderSerice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -24,4 +26,39 @@ public class OrderController {
     public List<WebOrder> getOrders(@AuthenticationPrincipal LocalUser user){
         return orderSerice.getOrders(user);
     }
+
+    //OK - LOGGEDUSER
+    @GetMapping("/{orderId}")
+    public ResponseEntity<WebOrder> getOrder(@PathVariable Long orderId, @AuthenticationPrincipal LocalUser user){
+        WebOrder order = orderSerice.getOrder(orderId);
+        if (order.getUser().getId() == user.getId()){
+            return ResponseEntity.ok(order);
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+    //ADMIN - OK
+    @GetMapping("/admin/{orderId}")
+    public ResponseEntity<WebOrder> getOrderAsAdmin(@PathVariable Long orderId){
+        return ResponseEntity.ok(orderSerice.getOrder(orderId));
+    }
+
+    //OK?
+    @PostMapping
+    public WebOrder createOrder(@RequestBody OrderBody body,
+                                                @AuthenticationPrincipal LocalUser user) throws IOException {
+        return orderSerice.createOrder(body, user);
+    }
+
+    //OK
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<WebOrder> deleteOrder(@PathVariable Long orderId,
+                                                @AuthenticationPrincipal LocalUser user){
+        if (orderSerice.getOrder(orderId).getUser().getId() == user.getId()){
+            orderSerice.deleteOrder(orderId);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
 }
--- src/main/java/com/ecommerce/ECommerceAPI/service/OrderSerice.java
+++ src/main/java/com/ecommerce/ECommerceAPI/service/OrderSerice.java
@@ -1,24 +1,85 @@
 package com.ecommerce.ECommerceAPI.service;
 
+import com.ecommerce.ECommerceAPI.api.model.OrderBody;
 import com.ecommerce.ECommerceAPI.model.LocalUser;
+import com.ecommerce.ECommerceAPI.model.Product;
 import com.ecommerce.ECommerceAPI.model.WebOrder;
+import com.ecommerce.ECommerceAPI.model.WebOrderQuantities;
+import com.ecommerce.ECommerceAPI.model.dao.AddressDAO;
 import com.ecommerce.ECommerceAPI.model.dao.WebOrderDAO;
+import com.ecommerce.ECommerceAPI.model.dao.WebOrderQuantitiesDAO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class OrderSerice {
 
     private WebOrderDAO webOrderDAO;
+    private WebOrderQuantitiesDAO webOrderQuantitiesDAO;
+    private ProductService productService;
+    private AddressDAO addressDAO;
 
-    public OrderSerice(WebOrderDAO webOrderDAO) {
+    public OrderSerice(WebOrderDAO webOrderDAO, WebOrderQuantitiesDAO webOrderQuantitiesDAO, ProductService productService, AddressDAO addressDAO) {
         this.webOrderDAO = webOrderDAO;
+        this.webOrderQuantitiesDAO = webOrderQuantitiesDAO;
+        this.productService = productService;
+        this.addressDAO = addressDAO;
     }
 
     public List<WebOrder> getOrders(LocalUser user){
         return webOrderDAO.findByUser(user);
     }
 
+    public WebOrder getOrder(Long id){
+        return webOrderDAO.findById(id).get();
+    }
 
+    //¿Meter los objetos address y quantities enteros o solo los integers? - Mejorable
+
+    public WebOrder createOrder(OrderBody body, LocalUser user) throws IOException {
+        WebOrder order = new WebOrder();
+        order.setUser(user);
+        order.setAddress(addressDAO.findById(body.getAddressId()).get());
+        Map<Long, Integer> quantities = body.getQuantities();
+        webOrderDAO.save(order);
+        for(Long productId : quantities.keySet()){
+            Product product = productService.getProduct(productId);
+            WebOrderQuantities webOrderQuantities = new WebOrderQuantities();
+            if (product.getInventory().getQuantity() < quantities.get(productId)){
+                return null;
+            }
+            webOrderQuantities.setQuantity(quantities.get(productId));
+            webOrderQuantities.setProduct(product);
+            webOrderQuantities.setOrder(order);
+            webOrderQuantitiesDAO.save(webOrderQuantities);
+        }
+        return order;
+    }
+
+//    public WebOrder createOrder(OrderBody body, LocalUser user){
+//        WebOrder order = new WebOrder();
+//        order.setUser(user);
+//        order.setAddress(body.getAddress());
+//        int size = body.getQuantities().size();
+//        for (int i = 0; i < size; i++){
+//            WebOrderQuantitiesDAO orderQuantities = body.getQuantities().get(i);
+//            Product product = orderQuantities.getProduct();
+//
+//            if (orderQuantities.getQuantity() >
+//                    product.getInventory().getQuantity()){
+//                return null;
+//            }
+//        }
+//        order.setQuantities(body.getQuantities());
+//        return webOrderDAO.save(order);
+//    }
+
+    public void deleteOrder(Long orderId){
+        webOrderDAO.deleteById(orderId);
+    }
 }
